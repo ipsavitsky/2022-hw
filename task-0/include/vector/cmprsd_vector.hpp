eb6741b7715d2839e8013500e1b32f1ae18f9c05
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-10-24 13:27:27 +0300

    added compressed vector code

--- task-0/include/vector/cmprsd_vector.hpp
+++ task-0/include/vector/cmprsd_vector.hpp
@@ -10,99 +10,66 @@ class compressed_vector {
     std::unordered_map<std::size_t, T> container;
     std::size_t vec_size;
 
-    double eps = 10e-7;
-
    public:
+    class matrix_equality_proxy {
+       private:
+        compressed_vector<T>* outer_vec;
+        std::size_t index;
+        double eps = 10e-7;
+
+       public:
+        matrix_equality_proxy(compressed_vector<T>* vec, std::size_t elem_num)
+            : outer_vec(vec), index(elem_num) {}
+
+        operator T() { return outer_vec->operator()(index); }
+
+        matrix_equality_proxy& operator=(T elem) {
+            if (elem > eps) {
+                outer_vec->container[index] = elem;
+            }
+            return *this;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os,
+                                        const matrix_equality_proxy& rn) {
+            os << rn;
+            return os;
+        }
+    };
+
     compressed_vector(std::size_t size) : vec_size(size) {}
 
     std::size_t get_size() { return vec_size; }
 
-    T& operator()(std::size_t access) {
+    matrix_equality_proxy& operator()(std::size_t access) {
         if (access >= vec_size) {
             throw out_of_bounds_exception();
         }
-        return container[access];
+        auto ret = matrix_equality_proxy(this, access); 
+        return ret;
     }
 
-    // template <typename L>
-    // concept can_sum = requires(T t, L l) {
-    //     { t + l } -> std::same_as<decltype(t + l)>;
-    // };
+    T at(std::size_t access) const {
+        if (access >= vec_size) {
+            throw out_of_bounds_exception();
+        }
+        return container.at(access);
+    }
 
-    template <typename M>
-    auto operator+(const compressed_vector<M>& other)
-        -> decltype(this->operator()(0) + other(0)) {
+    compressed_vector<T> operator+(const compressed_vector<T>& other) {
         if (vec_size != other.vec_size) {
             throw incorrect_dimensions();
         }
-        using res_elem_t = decltype(this->operator()(0) + other(0));
-        compressed_vector<res_elem_t> res_vec(vec_size);
+        compressed_vector<T> res_vec(vec_size);
         for (std::size_t i = 0; i < vec_size; ++i) {
-            res_elem_t res =
-                container.contains(i) && other.container.contains(i)
-                    ? container[i] + other(i)
-                : container.contains(i)       ? container[i]
-                : other.container.contains(i) ? other(i)
-                                              : 0;
+            T res = container.contains(i) && other.container.contains(i)
+                        ? container[i] + other.at(i)
+                    : container.contains(i)       ? container[i]
+                    : other.container.contains(i) ? other.at(i)
+                                                  : 0;
             res_vec(i) = res;
         }
-    }
-
-    template <typename M>
-    auto operator-(const compressed_vector<M>& other)
-        -> decltype(this->operator()(0) - other(0)) {
-        if (vec_size != other.vec_size) {
-            throw incorrect_dimensions();
-        }
-        using res_elem_t = decltype(this->operator()(0) - other(0));
-        compressed_vector<res_elem_t> res_vec(vec_size);
-        for (std::size_t i = 0; i < vec_size; ++i) {
-            res_elem_t res =
-                container.contains(i) && other.container.contains(i)
-                    ? container[i] - other(i)
-                : container.contains(i)       ? container[i]
-                : other.container.contains(i) ? other(i)
-                                              : 0;
-            res_vec(i) = res;
-        }
-    }
-
-    template <typename M>
-    auto operator*(const compressed_vector<M>& other)
-        -> decltype(this->operator()(0) * other(0)) {
-        if (vec_size != other.vec_size) {
-            throw incorrect_dimensions();
-        }
-        using res_elem_t = decltype(this->operator()(0) * other(0));
-        compressed_vector<res_elem_t> res_vec(vec_size);
-        for (std::size_t i = 0; i < vec_size; ++i) {
-            res_elem_t res =
-                container.contains(i) && other.container.contains(i)
-                    ? container[i] * other(i)
-                : container.contains(i)       ? container[i]
-                : other.container.contains(i) ? other(i)
-                                              : 0;
-            res_vec(i) = res;
-        }
-    }
-
-    template <typename M>
-    auto operator/(const compressed_vector<M>& other)
-        -> decltype(this->operator()(0) / other(0)) {
-        if (vec_size != other.vec_size) {
-            throw incorrect_dimensions();
-        }
-        using res_elem_t = decltype(this->operator()(0) / other(0));
-        compressed_vector<res_elem_t> res_vec(vec_size);
-        for (std::size_t i = 0; i < vec_size; ++i) {
-            res_elem_t res =
-                container.contains(i) && other.container.contains(i)
-                    ? container[i] / other(i)
-                : container.contains(i)       ? container[i]
-                : other.container.contains(i) ? other(i)
-                                              : 0;
-            res_vec(i) = res;
-        }
+        return res_vec;
     }
 
     compressed_vector<T> operator+(const T& additive) {
@@ -155,6 +122,14 @@ class compressed_vector {
             container[i] /= additive;
         }
         return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os,
+                                    const compressed_vector<T>& rn) {
+        for (int i = 0; i < rn.get_size(); ++i) {
+            os << rn(i);
+        }
+        return os;
     }
 };
 
--- task-0/test/vector_test.cpp
+++ task-0/test/vector_test.cpp
@@ -14,3 +14,22 @@ BOOST_AUTO_TEST_CASE(element_access) {
     BOOST_CHECK_EQUAL(vec(0), 3);
     BOOST_CHECK_EQUAL(vec(1), 0);
 }
+
+BOOST_AUTO_TEST_CASE(sum_of_vectors) {
+    compressed_vector<int> vec_1(5);
+    vec_1(0) = 2;
+    vec_1(2) = 1;
+    vec_1(3) = 8;
+    vec_1(4) = 9;
+    compressed_vector<int> vec_2(5);
+    vec_2(0) = 9;
+    vec_2(1) = 8;
+    vec_2(2) = 1;
+    vec_2(4) = 2;
+    auto res = vec_1 + vec_2;
+    BOOST_CHECK_EQUAL(res(0), 11);
+    BOOST_CHECK_EQUAL(res(1), 8);
+    BOOST_CHECK_EQUAL(res(2), 2);
+    BOOST_CHECK_EQUAL(res(3), 8);
+    BOOST_CHECK_EQUAL(res(4), 11);
+}
