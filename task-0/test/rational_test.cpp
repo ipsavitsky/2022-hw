b442197b90864f59269442c24152da001250d6b0
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-11-23 13:14:29 +0300

    task 0 restructured tests

--- task-0/test/rational_test.cpp
+++ task-0/test/rational_test.cpp
@@ -3,49 +3,45 @@
 #include <boost/test/unit_test.hpp>
 #include "rational/exceptions.hpp"
 
-BOOST_AUTO_TEST_CASE(equality) {
-    Rational_number a(1, 2);
-    Rational_number b(1, 2);
+struct rationals_fixture {
+    Rational_number a = Rational_number(1, 2);
+    Rational_number b = Rational_number(1, 2);
+    Rational_number c = Rational_number(2, 4);
+};
+
+BOOST_FIXTURE_TEST_CASE(equality, rationals_fixture) {
     BOOST_CHECK_EQUAL(a, b);
-    Rational_number c(2, 4);
     BOOST_CHECK_EQUAL(a, c);
     Rational_number d(15, 5);
     BOOST_CHECK_EQUAL(d, 3);
 }
 
-BOOST_AUTO_TEST_CASE(inequality) {
-    Rational_number a(1, 2);
+BOOST_FIXTURE_TEST_CASE(inequality, rationals_fixture) {
     Rational_number b(1, 3);
     BOOST_CHECK_NE(a, b);
-    Rational_number c(2, 4);
     BOOST_CHECK_NE(b, c);
     BOOST_CHECK_NE(5, c);
 }
 
-BOOST_AUTO_TEST_CASE(less) {
-    Rational_number a(1, 2);
+BOOST_FIXTURE_TEST_CASE(less, rationals_fixture) {
     Rational_number b(1, 3);
     BOOST_CHECK_LT(b, a);
-    Rational_number c(2, 4);
     BOOST_CHECK_LT(b, c);
     Rational_number d(100, 200);
     BOOST_CHECK_LE(c, d);
     BOOST_CHECK_LE(-1, d);
 }
 
-BOOST_AUTO_TEST_CASE(greater) {
-    Rational_number a(1, 2);
+BOOST_FIXTURE_TEST_CASE(greater, rationals_fixture) {
     Rational_number b(1, 3);
     BOOST_CHECK_GT(a, b);
-    Rational_number c(2, 4);
     BOOST_CHECK_GT(c, b);
     Rational_number d(100, 200);
     BOOST_CHECK_GE(c, d);
     BOOST_CHECK_GE(1, d);
 }
 
-BOOST_AUTO_TEST_CASE(infex_and_postfix) {
-    Rational_number a(1, 2);
+BOOST_FIXTURE_TEST_CASE(infex_and_postfix, rationals_fixture) {
     ++a;
     BOOST_CHECK_EQUAL(Rational_number(3, 2), a);
     BOOST_CHECK_EQUAL(Rational_number(3, 2), a++);
@@ -67,8 +63,7 @@ BOOST_AUTO_TEST_CASE(simplification) {
     BOOST_CHECK_EQUAL(5, b.get_denominator());
 }
 
-BOOST_AUTO_TEST_CASE(round_and_floor) {
-    Rational_number a(1, 2);
+BOOST_FIXTURE_TEST_CASE(round_and_floor, rationals_fixture) {
     BOOST_CHECK_EQUAL(Rational_number(0, 1), a.floor());
     Rational_number b(2, 3);
     BOOST_CHECK_EQUAL(Rational_number(1, 1), b.round());
@@ -82,8 +77,7 @@ BOOST_AUTO_TEST_CASE(convertions_to_integral) {
     BOOST_CHECK_EQUAL((short)a, 83);
 }
 
-BOOST_AUTO_TEST_CASE(arithmetics_with_numbers) {
-    Rational_number a(1, 2);
+BOOST_FIXTURE_TEST_CASE(arithmetics_with_numbers, rationals_fixture) {
     a += 5;
     BOOST_CHECK_EQUAL(Rational_number(11, 2), a);
     a -= 3;
--- task-0/test/vector_test.cpp
+++ task-0/test/vector_test.cpp
@@ -3,29 +3,34 @@
 #include "vector/cmprsd_vector.hpp"
 #include "vector/exceptions.hpp"
 
-BOOST_AUTO_TEST_CASE(out_of_bounds) {
-    compressed_vector<int> vec(5);
+struct vector_fixture {
+    compressed_vector<int> vec;
+    compressed_vector<int> vec_1;
+    compressed_vector<int> vec_2;
+
+    vector_fixture() : vec(5), vec_1(5), vec_2(5) {
+        vec_1(0) = 2;
+        vec_1(2) = 1;
+        vec_1(3) = 8;
+        vec_1(4) = 9;
+        vec_2(0) = 9;
+        vec_2(1) = 8;
+        vec_2(2) = 1;
+        vec_2(4) = 2;
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(out_of_bounds, vector_fixture) {
     BOOST_CHECK_THROW(vec(100), out_of_bounds_exception);
 }
 
-BOOST_AUTO_TEST_CASE(element_access) {
-    compressed_vector<int> vec(5);
+BOOST_FIXTURE_TEST_CASE(element_access, vector_fixture) {
     vec(0) = 3;
     BOOST_CHECK_EQUAL(vec(0), 3);
     BOOST_CHECK_EQUAL(vec(1), 0);
 }
 
-BOOST_AUTO_TEST_CASE(sum_of_vectors) {
-    compressed_vector<int> vec_1(5);
-    vec_1(0) = 2;
-    vec_1(2) = 1;
-    vec_1(3) = 8;
-    vec_1(4) = 9;
-    compressed_vector<int> vec_2(5);
-    vec_2(0) = 9;
-    vec_2(1) = 8;
-    vec_2(2) = 1;
-    vec_2(4) = 2;
+BOOST_FIXTURE_TEST_CASE(sum_of_vectors, vector_fixture) {
     auto res = vec_1 + vec_2;
     BOOST_CHECK_EQUAL(res(0), 11);
     BOOST_CHECK_EQUAL(res(1), 8);
